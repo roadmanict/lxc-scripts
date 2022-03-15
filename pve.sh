23737c3726430bdb125e1a323eb5bb2ad528bf7f
Geert Weggemans <geert@Geerts-MacBook-Pro.local>
2022-03-15 13:17:50 +0100

    Automatically install updates

--- pve.sh
+++ pve.sh
@@ -2,6 +2,6 @@
 rm /etc/apt/sources.list.d/pve-enterprise.list
 echo "deb http://download.proxmox.com/debian/pve bullseye pve-no-subscription" > /etc/sources.list.d/pve-no-subscription.list
 apt update
-apt full-upgrade
+apt full-upgrade --yes
 
 sed -Ezi.bak "s/(Ext.Msg.show\(\{\s+title: gettext\('No valid sub)/void\(\{ \/\/\1/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js && systemctl restart pveproxy.service

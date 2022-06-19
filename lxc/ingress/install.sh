90f47015447accaf7d7d6b6b6f6d42fd9095b4af
Geert Weggemans <geert@roadmanict.nl>
2022-06-19 18:29:47 +0200

    Add ingress install script

---
+++ lxc/ingress/install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+sudo apt update
+
+sudo apt install --yes nginx certbot python3-pip
+
+pip install certbot-dns-transip
+
+echo "Create transip.ini file with:"
+echo "dns_transip_username=Geertuh"
+echo "dns_transip_key_file=/etc/letsencrypt/transip-rsa.key"
+
+echo ""
+echo "Create transip.key file with private key from site"
+
+echo ""
+echo "Convert key: "
+echo "$ openssl rsa -in /etc/letsencrypt/transip.key -out /etc/letsencrypt/transip-rsa.key"
+echo "$ chmod 600 /etc/letsencrypt/transip-rsa.key "
+
+echo ""
+echo "Example command for generating certificates"
+echo "$ certbot certonly -n -d *.svc.gweggemans.nl -a dns-transip --dns-transip-credentials /etc/letsencrypt/transip.ini --dns-transip-propagation-seconds 240"

3274c213eff771359e7828eb51553b685df3e946
Geert Weggemans <git@relaylay.nl>
2024-03-19 10:38:54 +0100

    Improve bootstrap ansible

--- bootstrap-ansible.sh
+++ bootstrap-ansible.sh
@@ -15,12 +15,13 @@ rm -f /home/ansible/.ssh/authorized_keys
 touch /home/ansible/.ssh/authorized_keys
 
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDhjvmvRyaBojmOPhVS2EfmQLBllYVL8bhMRihxGOYqp lxc@debian" >> /home/ansible/.ssh/authorized_keys
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJYOk8j2fsAzz+TgYKJOvZtE+ncsj3U/XtQ4d37ql7r0 j.moes01@gmail.com" >> /home/ansible/.ssh/authorized_keys
 
 chmod 700 /home/ansible/.ssh
 chmod 600 /home/ansible/.ssh/authorized_keys
 
 chown -R ansible:ansible /home/ansible/.ssh
 
-echo "ansible ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
+echo "ansible ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers/ansible
 
 visudo -c
